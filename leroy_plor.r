500c8ef6fd2cbe5ac469e952dd4c5f8c5937e0a0
kamransafi <ksafi@ab.mpg.de>
2021-03-11 11:03:09 +0100

    bla bla

--- leroy_plor.r
+++ leroy_plor.r
@@ -1,5 +1,5 @@
 library(move)
 data <- leroy
-plot(leroy, type="l", lwd=2, main="Leroy", bty="n", xlab="Longitude", ylab="Latitude")
+plot(leroy, type="l", lwd=2, main="Leroy", bty="n", xlab=NA, ylab=NA)
 points(leroy, pch=20)
 points(leroy, pch=1, col="firebrick")
