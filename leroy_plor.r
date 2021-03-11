ffe6fe3b14370d2421e4c412cbc4f3565380c4f6
kamransafi <ksafi@ab.mpg.de>
2021-03-11 10:30:08 +0100

    fancier plot stuff

--- leroy_plor.r
+++ leroy_plor.r
@@ -1,3 +1,5 @@
 library(move)
 data <- leroy
-plot(leroy, type="l", lwd=2, main="Leroy")
+plot(leroy, type="l", lwd=2, main="Leroy", bty="n", xlab="Longitude", ylab="Latitude")
+points(leroy, pch=20)
+points(leroy, pch=1, col="firebrick")
