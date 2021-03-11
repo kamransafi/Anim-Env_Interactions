43cf9b54e61d39050647583755d44bef6ff84637
kamransafi <ksafi@ab.mpg.de>
2021-03-11 11:01:51 +0100

    got rid of Hannah's stuff

--- leroy_plor.r
+++ leroy_plor.r
@@ -1,11 +1,9 @@
 library(move)
 data <- leroy
-<<<<<<< HEAD
+
 plot(leroy, type="l", lwd=2, bty="n", xlab=NA, ylab=NA, xaxt="n", yaxt="n")
 title("Leroy")
 title(sub="Our dear fisher")
-=======
->>>>>>> 468de81d0b06d45f8b295c3a162789547b18a489
+
 points(leroy, pch=20)
-points(leroy, pch=1, col="firebrick")
 points(leroy, pch=1, col="firebrick")
