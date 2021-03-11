1bb90edf4a8db2bb35614634c4c94a5ad77a54da
kamransafi <ksafi@ab.mpg.de>
2021-03-11 10:56:02 +0100

    did some alterations

--- leroy_plor.r
+++ leroy_plor.r
@@ -1,5 +1,7 @@
 library(move)
 data <- leroy
-plot(leroy, type="l", lwd=2, main="Leroy", bty="n", xlab="Longitude", ylab="Latitude")
+plot(leroy, type="l", lwd=2, bty="n", xlab=NA, ylab=NA, xaxt="n", yaxt="n")
+title("Leroy")
+title(sub="Our dear fisher")
 points(leroy, pch=20)
 points(leroy, pch=1, col="firebrick")
