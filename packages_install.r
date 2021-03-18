83ba087047123b1776aa8752c5a6dcfac5801636
Kamran Safi <64833377+kamransafi@users.noreply.github.com>
2021-03-18 08:10:05 +0100

    Delete packages_install.r

--- packages_install.r
+++
@@ -1,7 +0,0 @@
-install.packages(c("rgdal", "shiny", "knitr", "sp", "MASS", "vegan", "mgcv", "move", 
-                   "raster", "RStoolbox", "httr", "RCurl", "mapdata", "lubridate", 
-                   "classInt", "scales", "circular", "fields", "plyr", "doMC", "readxl", 
-                   "shinydashboard", "ggplot2", "ggmap", "ndtv", "leaflet", "DT", "mapproj", 
-                   "data.table", "adehabitatHR", "adehabitatLT", "adehabitatHS", "ctmm", 
-                   "EMbC", "rgeos"), 
-                 dependencies=T, type="source")
